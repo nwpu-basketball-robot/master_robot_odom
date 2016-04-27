0b6064f447f1e965e3bb077a8ed15cc56c5b3df3
liao_zhi_han <cp_vin@126.com>
2016-04-27 15:22:36 +0800

    廖志涵，第一次提交

---
+++ include/basketball_odom/basketball_odom.h
@@ -0,0 +1,57 @@
+/*
+*Team Unware Basketball Robot NWPU
+*
+*接收下位机传上来的里程计数据，并发布在tf_tree中
+*
+*Author = liao-zhihan
+*
+*first_debug_date:2016-01-20
+*测试通过
+*/
+
+#ifndef BASKETBALL_ODOM
+#define BASKETBALL_ODOM
+
+#include <basketball_msgs/robot_state.h>
+#include <basketball_msgs/robot_message.h>
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+
+class RobotOdom
+{
+public:
+    RobotOdom(ros::NodeHandle &node) ;
+    ~RobotOdom() ;
+    void mainRun() ;
+protected:
+private:
+    //发布向odom话题发布数据
+    void pubOdom(const double x ,const double y ,const double yaw) ;
+    //下位机odom回调函数
+    void odomCallBack(const basketball_msgs::robot_state::ConstPtr &ptr) ;
+    //将odom数据发布在tf_tree中
+    void odomBroadcaster(const double x , const double y , const double yaw) ;
+    //向下位机请求里程计数据
+    void pubOdomRequest(const uint8_t func) ;
+    void setOdomVelocity(double d_x , double d_y , double d_z,nav_msgs::Odometry &odom) ;
+private:
+    ros::Time last_time_ ;
+    ros::NodeHandle nh_ ;
+    ros::Publisher odom_pub_ ;
+    ros::Publisher odom_request_pub_ ;
+    ros::Subscriber odom_data_sub_ ;
+    tf::TransformBroadcaster odom_broadcaster_ ;
+
+    double last_x_ ;
+    double last_y_ ;
+    double last_yaw_ ;
+
+    double current_x_ ;
+    double current_y_ ;
+    double current_yaw_ ;
+
+    int base_cmd_id_ ;
+} ;
+
+#endif // BASKETBALL_ODOM
---
+++ src/basketball_odom.cpp
@@ -0,0 +1,142 @@
+/*
+*Team Unware Basketball Robot NWPU
+*
+*接收下位机传上来的里程计数据，并发布在tf_tree中
+*
+*Author = liao-zhihan
+*
+*first_debug_date:2016-01-20
+*测试通过
+*/
+
+#include <basketball_odom/basketball_odom.h>
+#include <tf/tf.h>
+#include <sstream>
+#include <geometry_msgs/Quaternion.h>
+//#define DEBUG
+
+
+RobotOdom::RobotOdom(ros::NodeHandle &node):
+    nh_(node),
+    last_time_(ros::Time::now()),
+    last_x_(0.0),
+    last_y_(0.0),
+    last_yaw_(0.0),
+    current_x_(0.0),
+    current_y_(0.0),
+    current_yaw_(0.0),
+    base_cmd_id_(0x01)
+{
+    odom_pub_ = nh_.advertise<nav_msgs::Odometry>("odom", 50) ;
+    odom_request_pub_ = nh_.advertise<basketball_msgs::robot_message>("robot_cmd",10) ;
+    std::stringstream sub_name ;
+    sub_name<<"/RecvData/"<<(int)base_cmd_id_ ;
+    odom_data_sub_ = nh_.subscribe(sub_name.str(),100, &RobotOdom::odomCallBack,this) ;
+}
+
+RobotOdom::~RobotOdom()
+{
+    nh_.shutdown() ;
+}
+
+
+void RobotOdom::pubOdomRequest(const uint8_t func)
+{
+  //转化为下位机协议
+	basketball_msgs::robot_message robot_cmd_msg ;
+	robot_cmd_msg.data.resize(6 , 0) ;
+	uint8_t *data_ptr = robot_cmd_msg.data.data() ;
+	int data_len = 1 ;
+	data_ptr[0] = data_ptr[1] = 0xff ;
+	data_ptr[2] = base_cmd_id_ ;
+	data_ptr[3] = (u_int8_t)(data_len>>8) ;
+  data_ptr[4] = (u_int8_t)(data_len & 0xff) ;
+  data_ptr[5] = func ;
+  //转化完成
+	odom_request_pub_.publish(robot_cmd_msg) ;
+}
+
+void RobotOdom::setOdomVelocity(double d_x , double d_y , double d_z,nav_msgs::Odometry &odom)
+{
+    double d_time = (ros::Time::now() - last_time_).toSec() ;
+    double vx = d_x / d_time ;
+    double vy = d_y / d_time ;
+    double vth = d_z / d_time ;
+    odom.child_frame_id = "base_link";
+    odom.twist.twist.linear.x = vx;
+    odom.twist.twist.linear.y = vy;
+    odom.twist.twist.angular.z = vth;
+}
+
+void RobotOdom::mainRun()
+{
+    ros::Rate r(100) ;
+    while(ros::ok())
+    {
+        pubOdom(current_x_ , current_y_ , current_yaw_) ;
+        odomBroadcaster(current_x_ , current_y_ , current_yaw_) ;
+        last_x_ = current_x_ ;
+        last_y_ = current_y_ ;
+        last_yaw_ = current_yaw_ ;
+        last_time_ = ros::Time::now() ;
+	       pubOdomRequest(0x04) ;
+	        r.sleep() ;
+        ros::spinOnce() ;
+    }
+}
+
+void RobotOdom::pubOdom(const double x ,const double y ,const double yaw)
+{
+    nav_msgs::Odometry odom ;
+    odom.header.stamp = ros::Time::now() ;
+    odom.header.frame_id = "odom" ;
+    odom.pose.pose.position.x = x ;
+    odom.pose.pose.position.y = y ;
+    odom.pose.pose.position.z = 0.0 ;
+
+    odom.pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw) ;
+
+    setOdomVelocity(x - last_x_ , y - last_y_ , yaw - last_yaw_,odom) ;
+
+    odom_pub_.publish(odom) ;
+}
+
+//get the odometry data from handware and send these data to tf tree
+void RobotOdom::odomBroadcaster(const double x , const double y , const double yaw)
+{
+    geometry_msgs::TransformStamped odom_transform ;
+    odom_transform.header.stamp = ros::Time::now() ;
+
+    odom_transform.header.frame_id = "/odom" ;
+    odom_transform.child_frame_id = "/base_link" ;
+
+    odom_transform.transform.translation.x = x ;
+    odom_transform.transform.translation.y = y ;
+    odom_transform.transform.translation.z = 0.0 ;
+    odom_transform.transform.rotation = tf::createQuaternionMsgFromYaw(yaw) ;
+    odom_broadcaster_.sendTransform(odom_transform) ;
+}
+
+//get the odometry data from handware and send these data to navigation stack
+void RobotOdom::odomCallBack(const basketball_msgs::robot_state::ConstPtr &ptr)
+{
+    double x = ptr->data.at(0) ;
+    double y = ptr->data.at(1) ;
+    double yaw = ptr->data.at(2) ;
+    #ifdef DEBUG
+      ROS_INFO("the id is %d\n" , ptr->id) ;
+      ROS_INFO("current length is %d ,  current odom is x = %lf , y = %lf , yaw = %lf\n" , ptr->data.size(),  x , y ,yaw) ;
+    #endif
+    current_x_ = x ;
+    current_y_ = y ;
+    current_yaw_ = yaw ;
+}
+
+int main(int argc , char **argv)
+{
+    ros::init(argc , argv , "robot_odom") ;
+    ros::NodeHandle node ;
+    RobotOdom robot_odom(node) ;
+    robot_odom.mainRun() ;
+    return 0 ;
+}
